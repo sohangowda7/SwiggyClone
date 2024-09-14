70a15cf81e45fa4a097db978de95124472f25804
sohangowda7 <sohan.n2002@gmail.com>
2024-09-14 15:47:36 +0530

    ep1

---
+++ App.js
@@ -0,0 +1,9 @@
+
+const heading=React.createElement("div",{}, React.createElement("div",{}, React.createElement("h1",{}, "hello evryone")));
+
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+
+root.render(heading);
